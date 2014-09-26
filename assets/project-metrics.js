748d2f606627b1b28ca3391a4800df7c4dbc9b3a
Christian McDonald <cmcdonald@statesman.com>
2014-09-25 22:06:18 -0500

    first commit

---
+++ assets/project-metrics.js
@@ -0,0 +1,8 @@
+<!-- project specfic stuff -->
+<script language="JavaScript" type="text/javascript"><!--
+var projectSite = "statesman";
+var projectChannel = "features";
+var projectCategory = "local";
+var projectSubCategory = "hispanic";
+-->
+</script>
---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- working cache -->
+	<meta http-equiv="Cache-Control" content="max-age=3600, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
+
+	<meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Hispanic Culture in Austin">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<link rel="apple-touch-icon" href="assets/appicon.png">
+	<link rel="icon" type="image/png" href="assets/favicon.png">
+
+	<title>Hispanic Culture in Austin</title>
+	<meta name="description" content="Hispanic Culture in Austin: A survey of notable Latinos in Central Texas about the impact of Hispanics.">
+	<link rel="canonical" href="http://projects.statesman.com/features/hispanic-culture/" />
+	<meta property="og:title" content="Hispanic Culture in Austin"/>
+	<meta property="og:description" content="A survey of notable Latinos in Central Texas about the impact of Hispanics."/>
+	<meta property="og:image" content="assets/appicon.png"/>
+	<meta property="og:url" content="http://projects.statesman.com/features/hispanic-culture/" />
+
+	<link rel="stylesheet" href="http://projects.statesman.com/foundation/css/normalize.css">
+	<link rel="stylesheet" href="http://projects.statesman.com/foundation/css/foundation.css">
+
+<!-- project additions -->
+	<?php include "advertising.js";?>
+
+<!-- project-specific styles -->
+	<link rel="stylesheet" type="text/css" href="http://projects.statesman.com/foundation/slick/slick/slick.css"/>
+	<link rel="stylesheet" href="project.css">
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Bevan' rel='stylesheet' type='text/css'>
+
+<?php include "../../common/metrics-head.js";?>
+
+<!-- page additions -->
+<style>
+		ul.tabs{
+			margin: 0px;
+			padding: 0px;
+			list-style: none;
+		}
+		ul.tabs li{
+			background: none;
+			color: #222;
+			display: inline-block;
+			padding: 10px 15px;
+			cursor: pointer;
+		}
+
+		ul.tabs li.current{
+			background: #ededed;
+			color: #222;
+		}
+
+		.tab-content{
+			display: none;
+			background: #ededed;
+			padding: 15px;
+		}
+
+		.tab-content.current{
+			display: inherit;
+		}
+</style>
+</head>
+<body>
+
+<div class="fixed sticky" data-options="sticky_on: large">
+	<nav class="top-bar" data-topbar>
+		<ul class="title-area">
+			<li class="name"><h1><a href="http://www.mystatesman.com" target="_blank"><img src="http://projects.statesman.com/common/logo-white.svg" /></a></h1></li>
+		</ul>
+
+		<section class="top-bar-section">
+			<!-- Right Nav Section -->
+			<ul class="right">
+				<li class="has-dropdown"><a href="#">CREDITS</a>
+						<ul class="dropdown">
+							<li><a href="mailto:rvillalpando@statesman.com">Interactive production: Roberto Villalpando</a></li>
+						</ul>
+				</li>
+			</ul>
+		</section>
+	</nav>
+</div>
+
+
+<a name="ad01" id="top"></a>
+<?php include "advertising.php";?>
+
+
+<div class="row">
+	<div class="large-11 medium-11 small-11 large-centered medium-centered small-centered columns">
+		<div class="sliderContent" id="answer-items">
+			<div><a href="avila.html"><img src="images/avila.jpg" /><h5>Mike<br>Avila</h5><p class="hide-for-small">Webmaster for Texas State Library and Archives Commission</p></a></div>
+            <div><a href="camacho.html"><img src="images/camacho.jpg" /><h5>Danny<br>Camacho</h5><p class="hide-for-small">Community historian</p></a></div>
+            <div><a href="campos.html"><img src="images/campos.jpg" /><h5>Olga<br>Campos</h5><p class="hide-for-small">Charity leader, former TV anchor</p></a></div>
+            <div><a href="castro.html"><img src="images/castro.jpg" /><h5>Zeke<br>Castro</h5><p class="hide-for-small">Musician/educator</p></a></div>
+            <div><a href="cavasos.html"><img src="images/cavasos.jpg" /><h5>Perla<br>Cavasos</h5><p class="hide-for-small">Legislative director<br>for State Sen. Sylvia Garcia</p></a></div>
+            <div><a href="chavez.html"><img src="images/chavez.jpg" /><h5>Gina<br>Chavez</h5><p class="hide-for-small">Musician</p></a></div>
+            <div><a href="duran.html"><img src="images/duran.jpg" /><h5>Fidencio<br>Duran</h5><p class="hide-for-small">Artist</p></a></div>
+            <div><a href="flores.html"><img src="images/flores.jpg" /><h5>'Lulu'<br>Flores</h5><p class="hide-for-small">Attorney</p></a></div>
+            <div><a href="garcia.html"><img src="images/garcia.jpg" /><h5>Arnold<br>Garcia</h5><p class="hide-for-small">Former American-Statesman editorial page editor</p></a></div>
+            <div><a href="ggarcia.html"><img src="images/ggarcia.jpg" /><h5>Gus<br>Garcia</h5><p class="hide-for-small">Former Austin mayor and city council member</p></a></div>
+            <div><a href="jgarza.html"><img src="images/jgarza.jpg" /><h5>Jesus<br>Garza</h5><p class="hide-for-small">President/CEO Seton Healthcare Family</p></a></div>
+            <div><a href="rgarza.html"><img src="images/rgarza.jpg" /><h5>Rich<br>Garza</h5><p class="hide-for-small">Director of Pachanga Fest</p></a></div>
+            <div><a href="gomez.html"><img src="images/gomez.jpg" /><h5>Margaret<br>Gomez</h5><p class="hide-for-small">Travis County commissioner, precinct 4</p></a></div>
+            <div><a href="llimon.html"><img src="images/llimon.jpg" /><h5>Lonnie<br>Limon</h5><p class="hide-for-small">Marketing executive</p></a></div>
+            <div><a href="mlimon.html"><img src="images/mlimon.jpg" /><h5>Marisa<br>Limon</h5><p class="hide-for-small">Nonprofit and education consultant</p></a></div>
+            <div><a href="lozano.html"><img src="images/lozano.jpg" /><h5>Vicente<br>Lozano</h5><p class="hide-for-small">Writer</p></a></div>
+            <div><a href="moya.html"><img src="images/moya.jpg" /><h5>Richard<br>Moya</h5><p class="hide-for-small">Former Travis County Commissioner</p></a></div>
+            <div><a href="orozco.html"><img src="images/orozco.jpg" /><h5>Sylvia<br>Orozco</h5><p class="hide-for-small">Director of Mexic-Arte Museum</p></a></div>
+            <div><a href="peraza.html"><img src="images/peraza.jpg" /><h5>Monica<br>Peraza</h5><p class="hide-for-small">Entrepreneur and philanthropist</p></a></div>
+            <div><a href="ramirez.html"><img src="images/ramirez.jpg" /><h5>Victoria<br>Ramirez</h5><p class="hide-for-small">Director of Bullock Texas State History Museum</p></a></div>
+            <div><a href="rivas-rodriguez.html"><img src="images/rivas-rodriguez.jpg" /><h5>Maggie<br>Rivas-Rodriguez</h5><p class="hide-for-small">U.S. Latino &amp; Latina Oral History Project director</p></a></div>
+            <div><a href="rodriguez.html"><img src="images/rodriguez.jpg" /><h5>Letitia<br>Rodriguez</h5><p class="hide-for-small">Musician</p></a></div>
+            <div><a href="rodriguez-mendoza.html"><img src="images/rodriguez-mendoza.jpg" /><h5>Amalia<br>Rodriguez-Mendoza</h5><p class="hide-for-small">District clerk of Travis County</p></a></div>
+            <div><a href="rosereyes.html"><img src="images/rosereyes.jpg" /><h5>Rose<br>Reyes</h5><p class="hide-for-small">Chief Operations Officer at Giant Noise</p></a></div>
+			<div><a href="rupertreyes.html"><img src="images/rupertreyes.jpg" /><h5>Rupert<br>Reyes</h5><p class="hide-for-small">Playwright, co-director Teatro Vivo</p></a></div>
+            <div><a href="rosas.html"><img src="images/rosas.jpg" /><h5>Lilia<br>Rosas</h5><p class="hide-for-small">Director of Red Salmon Arts</p></a></div>
+            <div><a href="saenz.html"><img src="images/saenz.jpg" /><h5>Erica<br>Saenz</h5><p class="hide-for-small">UT community engagement<br>specialist</p></a></div>
+            <div><a href="salinas.html"><img src="images/salinas.jpg" /><h5>Roen<br>Salinas</h5><p class="hide-for-small">Director of Aztlan Dance Company</p></a></div>
+            <div><a href="sepulveda.html"><img src="images/sepulveda.jpg" /><h5>Eugene<br>Sepulveda</h5><p class="hide-for-small">Social entrepreneur and philanthropist</p></a></div>
+            <div><a href="sifuentes.html"><img src="images/sifuentes.jpg" /><h5>Ben<br>Sifuentes</h5><p class="hide-for-small">Retired health care professional</p></a></div>
+            <div><a href="torres.html"><img src="images/torres.jpg" /><h5>Milton<br>Torres</h5><p class="hide-for-small">Publisher of Popular Hispanics magazine</p></a></div>
+            <div><a href="tzinzun.html"><img src="images/tzinzun.jpg" /><h5>Cristina<br>Tzinzun</h5><p class="hide-for-small">Workers Defense Project director</p></a></div>
+            <div><a href="zambrano.html"><img src="images/zambrano.jpg" /><h5>Armando<br>Zambrano</h5><p class="hide-for-small">Fundraiser for Children's Medical Center Foundation of Central Texas</p></a></div>
+            <div><a href="zamora.html"><img src="images/zamora.jpg" /><h5>Emilio<br>Zamora</h5><p class="hide-for-small">Historian</p></a></div>
+		</div>
+
+		<div id="answer-description">
+			<h3>Choose a person from the gallery above</h3>
+            <p class="lead">We asked three questions:</p>
+            <ul>
+            	<li>1. What is the <b>biggest impact</b> of Hispanic culture on Austin?</li>
+                <li>2. What is the <b>least understood impact</b> of that culture here?</li>
+                <li>3. Most importantly, what will be in the <b>impact of Hispanic culture on Austin's future</b>?</li>
+            </ul>
+		</div>
+	</div>
+</div>
+
+<!-- small sharing because stock add this goes away -->
+<div class="row show-for-small-only">
+	<div class="small-12 columns small-centered">
+	<!-- AddThis Button BEGIN -->
+	<div class="addthis_toolbox addthis_default_style addthis_32x32_style small-12 small-centered columns">
+	<a class="addthis_button_facebook"></a>
+	<a class="addthis_button_twitter"></a>
+	<a class="addthis_button_google_plusone_share"></a>
+	<a class="addthis_button_email"></a>
+	<a class="addthis_button_compact"></a>
+	</div>
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52f93567099a6a05"></script>
+	<!-- AddThis Button END -->
+	</div>
+ </div>
+
+<!-- stock add this. this is tied to the account and will show anytime you you have addthis script () -->
+<div class="show-for-medium-up">
+	<!-- Go to www.addthis.com/dashboard to customize your tools -->
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52f93567099a6a05"></script>
+</div>
+
+<div id="introModal" class="reveal-modal small" data-reveal>
+	<div class="large-12 columns">
+    	<h1>What is the impact of Hispanic culture in Austin?</h1>	
+		<p class="lead">We asked dozens of notable Latinos in Central Texas for their thoughts and opinions. Choose from the scrolling gallery of Austinites to read that person's answer.</p>
+    </div>
+	<a class="close-reveal-modal">&#215;</a>
+</div>
+
+<script src="http://projects.statesman.com/foundation/js/vendor/jquery.js"></script>
+<script src="http://projects.statesman.com/foundation/js/foundation.min.js"></script>
+  
+   <script>
+    $(document).foundation();
+  </script>
+   
+    <script>
+ $("#introModal").foundation('reveal', 'open');
+ </script>
+ 
+<script type="text/javascript" src="http://projects.statesman.com/foundation/slick/slick/slick.min.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.sliderContent').slick({
+			dots: true,
+			infinite: false,
+			speed: 300,
+			slidesToShow: 6,
+			slidesToScroll: 6,
+			responsive: [
+				{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 5,
+					infinite: true,
+					dots: true
+					}
+				},
+				{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 4
+					}
+				},
+				{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3
+					}
+				}
+			]
+		});
+	});
+</script>
+    
+ <script>
+$(document).ready(function(e) {
+
+var $answers = $('#answer-items');
+
+$('#answer-items').find('a').on('click', function(e){
+	e.preventDefault();
+	
+	var $desc = $('#answer-description');
+	
+	switch($(this).attr('href')) {
+		case 'rodriguez-mendoza.html' :
+			$desc.load('answers/rodriguez-mendoza.html');
+			break;
+		case 'garcia.html' :
+			$desc.load('answers/garcia.html');
+			break;
+		case 'sifuentes.html' :
+			$desc.load('answers/sifuentes.html');
+			break;
+		case 'tzinzun.html' :
+			$desc.load('answers/tzinzun.html');
+			break;
+		case 'camacho.html' :
+			$desc.load('answers/camacho.html');
+			break;
+		case 'zamora.html' :
+			$desc.load('answers/zamora.html');
+			break;
+		case 'saenz.html' :
+			$desc.load('answers/saenz.html');
+			break;
+		case 'sepulveda.html' :
+			$desc.load('answers/sepulveda.html');
+			break;
+		case 'duran.html' :
+			$desc.load('answers/duran.html');
+			break;
+		case 'jgarza.html' :
+			$desc.load('answers/jgarza.html');
+			break;
+		case 'rodriguez.html' :
+			$desc.load('answers/rodriguez.html');
+			break;
+		case 'rosas.html' :
+			$desc.load('answers/rosas.html');
+			break;
+		case 'llimon.html' :
+			$desc.load('answers/llimon.html');
+			break;
+		case 'rivas-rodriguez.html' :
+			$desc.load('answers/rivas-rodriguez.html');
+			break;
+		case 'flores.html' :
+			$desc.load('answers/flores.html');
+			break;
+		case 'mlimon.html' :
+			$desc.load('answers/mlimon.html');
+			break;
+		case 'avila.html' :
+			$desc.load('answers/avila.html');
+			break;
+		case 'torres.html' :
+			$desc.load('answers/torres.html');
+			break;
+		case 'peraza.html' :
+			$desc.load('answers/peraza.html');
+			break;
+		case 'campos.html' :
+			$desc.load('answers/campos.html');
+			break;
+		case 'cavasos.html' :
+			$desc.load('answers/cavasos.html');
+			break;
+		case 'rgarza.html' :
+			$desc.load('answers/rgarza.html');
+			break;
+		case 'moya.html' :
+			$desc.load('answers/moya.html');
+			break;
+		case 'salinas.html' :
+			$desc.load('answers/salinas.html');
+			break;
+		case 'rosereyes.html' :
+			$desc.load('answers/rosereyes.html');
+			break;
+		case 'rupertreyes.html' :
+			$desc.load('answers/rupertreyes.html');
+			break;
+		case 'orozco.html' :
+			$desc.load('answers/orozco.html');
+			break;
+		case 'ramirez.html' :
+			$desc.load('answers/ramirez.html');
+			break;
+		case 'castro.html' :
+			$desc.load('answers/castro.html');
+			break;
+		case 'lozano.html' :
+			$desc.load('answers/lozano.html');
+			break;
+		case 'chavez.html' :
+			$desc.load('answers/chavez.html');
+			break;
+		case 'zambrano.html' :
+			$desc.load('answers/zambrano.html');
+			break;
+		case 'gomez.html' :
+			$desc.load('answers/gomez.html');
+			break;
+		case 'ggarcia.html' :
+			$desc.load('answers/ggarcia.html');
+			break;
+	}
+});
+
+});
+</script>
+
+<!-- metrics code -->
+<?php include "assets/project-metrics.js"; ?>
+<?php include "../../common/metrics.js"; ?>
+</body>
+</html>
